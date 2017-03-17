2e7b2446881673450914a24619482332d1a35254
objektwerks <objektwerks@runbox.com>
2017-03-17 09:00:35 -0400

    add sys shutdown hook and exit menu action

--- src/main/scala/hs/App.scala
+++ src/main/scala/hs/App.scala
@@ -4,7 +4,7 @@ import com.typesafe.config.ConfigFactory
 import slick.basic.DatabaseConfig
 import slick.jdbc.{H2Profile, JdbcProfile}
 
-import scalafx.application.JFXApp
+import scalafx.application.{JFXApp, Platform}
 import scalafx.collections.ObservableBuffer
 import scalafx.geometry.Insets
 import scalafx.scene.Scene
@@ -16,14 +16,21 @@ object App extends JFXApp {
   val repository = new Repository(config = config, profile = H2Profile)
   import repository._
 
+  sys.addShutdownHook { closeRepository() }
+
+  val loadTestDataMenuItem = new MenuItem("Load Test Data")
+  val exitMenuItem = new MenuItem("Exit") { onAction = { _ => Platform.exit() } }
+  val fileMenu = new Menu("File") { items = List(loadTestDataMenuItem, exitMenuItem) }
+  val menuBar = new MenuBar { menus = List(fileMenu); useSystemMenuBar = true }
+
   val studentLabel = new Label { text = "Student:" }
-  val studentComboBox = new ComboBox[Student] { prefHeight = 25; prefWidth = 200; items = ObservableBuffer[Student]() }
+  val studentComboBox = new ComboBox[Student] { prefHeight = 25; prefWidth = 210; items = ObservableBuffer[Student]() }
   val studentPropsButton = new Button { text = "*"; prefHeight = 25 }
   val studentAddButton = new Button { text = "+"; prefHeight = 25 }
   val studentPane = new HBox { spacing = 6; children = List(studentLabel, studentComboBox, studentPropsButton, studentAddButton) }
 
   val gradeLabel = new Label { text = "Grade:" }
-  val gradeComboBox = new ComboBox[Grade] { prefHeight = 25; prefWidth = 90; items = ObservableBuffer[Grade]() }
+  val gradeComboBox = new ComboBox[Grade] { prefHeight = 25; prefWidth = 60; items = ObservableBuffer[Grade]() }
   val gradePropsButton = new Button { text = "*"; prefHeight = 25 }
   val gradeAddButton = new Button { text = "+"; prefHeight = 25 }
   val gradePane = new HBox { spacing = 6; children = List(gradeLabel, gradeComboBox, gradePropsButton, gradeAddButton) }
@@ -31,17 +38,17 @@ object App extends JFXApp {
   val studentGradePane = new HBox { spacing = 6; children = List(studentPane, gradePane) }
 
   val coursesLabel = new Label { text = "Courses:" }
-  val coursesList = new ListView[Course] { prefWidth = 300; items = ObservableBuffer[Course]() }
+  val coursesList = new ListView[Course] { prefWidth = 333; items = ObservableBuffer[Course]() }
   val coursesPropsButton = new Button { text = "*" }
   val coursesAddButton = new Button { text = "+" }
   val coursesToolBar = new HBox { spacing = 6; children = List(coursesPropsButton, coursesAddButton)}
   val coursesPane = new VBox { spacing = 6; children = List(coursesLabel, coursesList, coursesToolBar) }
 
   val assignmentsLabel = new Label { text = "Assignments:" }
-  val assignmentsList = new ListView[Assignment] { prefWidth = 300; items = ObservableBuffer[Assignment]() }
-  val assignedDate = new Label()
+  val assignmentsList = new ListView[Assignment] { prefWidth = 333; items = ObservableBuffer[Assignment]() }
+  val assignedDate = new Label { text = "00/00/0000" }
   val toLabel = new Label { text = " - " }
-  val completedDate = new Label()
+  val completedDate = new Label { text = "00/00/0000" }
   val scoreLabel = new Label { text = "0.0" }
   val splitLabel = new Label { text = " / " }
   val totalLabel = new Label { text = "0.0" }
@@ -54,6 +61,7 @@ object App extends JFXApp {
   val northPane = new HBox { spacing = 6; children = List(studentGradePane) }
   val separatorPane = new Separator()
   val southPane = new HBox { spacing = 6; children = List(coursesPane, assignmentsPane) }
-  val contentPane = new VBox { spacing = 6; padding = Insets(6); prefWidth = 600; children = List(northPane, separatorPane, southPane) }
+  val contentPane = new VBox { spacing = 6; padding = Insets(6); children = List(menuBar, northPane, separatorPane, southPane)}
+
   stage = new JFXApp.PrimaryStage { scene = new Scene { root = contentPane; title = "Homeschool"} }
 }
