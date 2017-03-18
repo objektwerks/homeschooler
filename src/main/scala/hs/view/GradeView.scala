20b5dfe08db12cf86c4e3a2ddef3355de4d0e5c4
objektwerks <objektwerks@runbox.com>
2017-03-18 10:00:35 -0400

    reformatted code

--- src/main/scala/hs/view/GradeView.scala
+++ src/main/scala/hs/view/GradeView.scala
@@ -10,7 +10,7 @@ import scalafx.util.StringConverter
 
 object GradeView {
   val gradeLabel = new Label { text = "Grade:" }
-  val gradeCellFactory = TextFieldListCell.forListView( StringConverter.toStringConverter[Grade](g => g.grade.toString ) )
+  val gradeCellFactory = TextFieldListCell.forListView( StringConverter.toStringConverter[Grade](g => g.grade.toString) )
   val gradeComboBox = new ComboBox[Grade] { prefHeight = 25; prefWidth = 60; items = ObservableBuffer[Grade](); cellFactory = gradeCellFactory }
   val gradePropsButton = new Button { text = "*"; prefHeight = 25 }
   val gradeAddButton = new Button { text = "+"; prefHeight = 25 }
