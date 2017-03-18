77877d642f7f7b4a14f9229713f22cbd1c1dbbdb
objektwerks <objektwerks@runbox.com>
2017-03-18 09:55:17 -0400

    added grade cell factory

--- src/main/scala/hs/view/GradeView.scala
+++ src/main/scala/hs/view/GradeView.scala
@@ -3,12 +3,15 @@ package hs.view
 import hs.domain.Grade
 
 import scalafx.collections.ObservableBuffer
+import scalafx.scene.control.cell.TextFieldListCell
 import scalafx.scene.control.{Button, ComboBox, Label}
 import scalafx.scene.layout.HBox
+import scalafx.util.StringConverter
 
 object GradeView {
   val gradeLabel = new Label { text = "Grade:" }
-  val gradeComboBox = new ComboBox[Grade] { prefHeight = 25; prefWidth = 60; items = ObservableBuffer[Grade]() }
+  val gradeCellFactory = TextFieldListCell.forListView( StringConverter.toStringConverter[Grade](g => g.grade.toString ) )
+  val gradeComboBox = new ComboBox[Grade] { prefHeight = 25; prefWidth = 60; items = ObservableBuffer[Grade](); cellFactory = gradeCellFactory }
   val gradePropsButton = new Button { text = "*"; prefHeight = 25 }
   val gradeAddButton = new Button { text = "+"; prefHeight = 25 }
   val gradePane = new HBox { spacing = 6; children = List(gradeLabel, gradeComboBox, gradePropsButton, gradeAddButton) }
