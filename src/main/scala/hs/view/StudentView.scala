4f5eeacb3a5e33003c65007f4735a714a29dcb71
objektwerks <objektwerks@runbox.com>
2017-03-18 09:42:58 -0400

    added student cell factory

--- src/main/scala/hs/view/StudentView.scala
+++ src/main/scala/hs/view/StudentView.scala
@@ -3,12 +3,15 @@ package hs.view
 import hs.domain.Student
 
 import scalafx.collections.ObservableBuffer
+import scalafx.scene.control.cell.TextFieldListCell
 import scalafx.scene.control.{Button, ComboBox, Label}
 import scalafx.scene.layout.HBox
+import scalafx.util.StringConverter
 
 object StudentView {
   val studentLabel = new Label { text = "Student:" }
-  val studentComboBox = new ComboBox[Student] { prefHeight = 25; prefWidth = 203; items = ObservableBuffer[Student]() }
+  val studentCellFactory = TextFieldListCell.forListView( StringConverter.toStringConverter[Student]( { s => s.name } ) )
+  val studentComboBox = new ComboBox[Student] { prefHeight = 25; prefWidth = 203; items = ObservableBuffer[Student](); cellFactory = studentCellFactory }
   val studentPropsButton = new Button { text = "*"; prefHeight = 25 }
   val studentAddButton = new Button { text = "+"; prefHeight = 25 }
   val studentPane = new HBox { spacing = 6; children = List(studentLabel, studentComboBox, studentPropsButton, studentAddButton) }
