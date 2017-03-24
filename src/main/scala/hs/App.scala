07e34b54962b59d17f79ce54eea356ca4fae740b
objektwerks <objektwerks@runbox.com>
2017-03-24 09:03:59 -0400

    refactored app pane assembly

--- src/main/scala/hs/App.scala
+++ src/main/scala/hs/App.scala
@@ -15,15 +15,10 @@ import scalafx.scene.layout.{HBox, VBox}
 object App extends JFXApp {
   val config = DatabaseConfig.forConfig[JdbcProfile]("app", ConfigFactory.load("app.conf"))
   val repository = new Repository(config = config, profile = H2Profile)
-  val menuPane = new MenuPane()
-  val studentPane = new StudentPane()
-  val gradePane = new GradePane()
-  val coursePane = new CoursePane()
-  val assignmentPane = new AssignmentPane()
 
-  val northPane = new HBox { spacing = 6; children = List(studentPane, new Separator { orientation = Orientation.Vertical }, gradePane) }
-  val southPane = new HBox { spacing = 6; children = List(coursePane, assignmentPane) }
-  val contentPane = new VBox { spacing = 6; padding = Insets(6); children = List(menuPane, northPane, new Separator(), southPane)}
+  val northPane = new HBox { spacing = 6; children = List(new StudentPane(), new Separator { orientation = Orientation.Vertical }, new GradePane()) }
+  val southPane = new HBox { spacing = 6; children = List(new CoursePane(), new AssignmentPane()) }
+  val contentPane = new VBox { spacing = 6; padding = Insets(6); children = List(new MenuPane(), northPane, new Separator(), southPane)}
   val sceneGraph = new Scene { root = contentPane }
   stage = new JFXApp.PrimaryStage { scene = sceneGraph; title = "Homeschool"; minHeight = 516; maxHeight = 516; minWidth = 684; maxWidth = 684 }
 
