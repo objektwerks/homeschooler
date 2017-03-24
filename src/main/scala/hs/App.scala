0612bd1380b85dbcceb6e9cb90cafc99def2cda5
objektwerks <objektwerks@runbox.com>
2017-03-24 09:22:41 -0400

    added app.conf for app

--- src/main/scala/hs/App.scala
+++ src/main/scala/hs/App.scala
@@ -1,5 +1,6 @@
 package hs
 
+import com.typesafe.config.ConfigFactory
 import hs.pane._
 
 import scalafx.application.JFXApp
@@ -9,11 +10,13 @@ import scalafx.scene.control._
 import scalafx.scene.layout.{HBox, VBox}
 
 object App extends JFXApp {
+  val conf = ConfigFactory.load("app.conf")
+
   val northPane = new HBox { spacing = 6; children = List(new StudentPane(), new Separator { orientation = Orientation.Vertical }, new GradePane()) }
   val southPane = new HBox { spacing = 6; children = List(new CoursePane(), new AssignmentPane()) }
   val contentPane = new VBox { spacing = 6; padding = Insets(6); children = List(new MenuPane(), northPane, new Separator(), southPane) }
   val sceneGraph = new Scene { root = contentPane }
-  stage = new JFXApp.PrimaryStage { scene = sceneGraph; title = "Homeschool"; minHeight = 516; maxHeight = 516; minWidth = 684; maxWidth = 684 }
+  stage = new JFXApp.PrimaryStage { scene = sceneGraph; title = conf.getString("title"); minHeight = 516; maxHeight = 516; minWidth = 684; maxWidth = 684 }
 
   sys.addShutdownHook { Store.repository.close() }
 }
