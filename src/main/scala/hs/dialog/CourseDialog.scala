a6e5e8bd1af9bb6fec657bfae9ccb59283107e28
objektwerks <objektwerks@runbox.com>
2017-03-24 08:20:02 -0400

    built course dialog

--- src/main/scala/hs/dialog/CourseDialog.scala
+++ src/main/scala/hs/dialog/CourseDialog.scala
@@ -1,12 +1,27 @@
 package hs.dialog
 
 import hs.App
+import hs.pane.ControlGridPane
 import hs.repository.Course
 
-import scalafx.scene.control.Dialog
+import scalafx.scene.control.ButtonBar.ButtonData
+import scalafx.scene.control._
 
 class CourseDialog(course: Course) extends Dialog[Course]() {
+  val saveButtonType = new ButtonType("Save", ButtonData.OKDone)
+  val nameTextField = new TextField { text = course.name}
+  val gridPaneControls = Map[String, Control]("Name:" -> nameTextField)
+  val gridPane = new ControlGridPane(gridPaneControls)
+
+  dialogPane().getButtonTypes.addAll(saveButtonType, ButtonType.Cancel)
+  dialogPane().contentProperty().set(gridPane)
   initOwner(App.stage)
   title = "Course"
   headerText = "Save Course"
+
+  resultConverter = dialogButton => {
+    if (dialogButton == saveButtonType)
+      course.copy(name = nameTextField.text.value)
+    else null
+  }
 }
