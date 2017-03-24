ac3cdf37c5bf02ffd91a1999fda73afb8f72cea9
objektwerks <objektwerks@runbox.com>
2017-03-24 09:04:38 -0400

    reformatted code

--- src/main/scala/hs/App.scala
+++ src/main/scala/hs/App.scala
@@ -18,7 +18,7 @@ object App extends JFXApp {
 
   val northPane = new HBox { spacing = 6; children = List(new StudentPane(), new Separator { orientation = Orientation.Vertical }, new GradePane()) }
   val southPane = new HBox { spacing = 6; children = List(new CoursePane(), new AssignmentPane()) }
-  val contentPane = new VBox { spacing = 6; padding = Insets(6); children = List(new MenuPane(), northPane, new Separator(), southPane)}
+  val contentPane = new VBox { spacing = 6; padding = Insets(6); children = List(new MenuPane(), northPane, new Separator(), southPane) }
   val sceneGraph = new Scene { root = contentPane }
   stage = new JFXApp.PrimaryStage { scene = sceneGraph; title = "Homeschool"; minHeight = 516; maxHeight = 516; minWidth = 684; maxWidth = 684 }
 
