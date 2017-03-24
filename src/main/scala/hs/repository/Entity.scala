f741e0d63b64cbb61d383f7e40beed6f6bcd1483
objektwerks <objektwerks@runbox.com>
2017-03-24 11:34:35 -0400

    refactored entities to provide defaults for ui controls

--- src/main/scala/hs/repository/Entity.scala
+++ src/main/scala/hs/repository/Entity.scala
@@ -2,7 +2,7 @@ package hs.repository
 
 import java.time.LocalDate
 
-case class Student(id: Int = 0, name: String, born: LocalDate)
-case class Grade(id: Int = 0, studentid: Int, year: String, started: LocalDate = LocalDate.now, completed: LocalDate = LocalDate.now.plusMonths(6))
-case class Course(id: Int = 0, gradeid: Int, name: String)
-case class Assignment(id: Int = 0, courseid: Int, task: String, assigned: LocalDate = LocalDate.now, completed: LocalDate = LocalDate.now, score: Double = 0.0)
+case class Student(id: Int = 0, name: String = "Name?", born: LocalDate = LocalDate.now.minusYears(7))
+case class Grade(id: Int = 0, studentid: Int, year: String = "Year?", started: LocalDate = LocalDate.now, completed: LocalDate = LocalDate.now.plusMonths(6))
+case class Course(id: Int = 0, gradeid: Int, name: String = "Name?")
+case class Assignment(id: Int = 0, courseid: Int, task: String = "Task?", assigned: LocalDate = LocalDate.now, completed: LocalDate = LocalDate.now, score: Double = 0.0)
