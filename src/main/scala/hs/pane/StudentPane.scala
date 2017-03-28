476121782145e351b2988adb15d7a7ffc8abb8a5
objektwerks <objektwerks@runbox.com>
2017-03-28 08:14:14 -0400

    refactored student pane event and model code

--- src/main/scala/hs/pane/StudentPane.scala
+++ src/main/scala/hs/pane/StudentPane.scala
@@ -1,8 +1,9 @@
 package hs.pane
 
+import hs.Model
+import hs.Store.repository._
 import hs.dialog.StudentDialog
 import hs.repository.Student
-import hs.{Model, Store}
 
 import scalafx.scene.control.cell.TextFieldListCell
 import scalafx.scene.control.{Button, ComboBox, Label}
@@ -20,23 +21,36 @@ class StudentPane extends HBox {
   children = List(studentLabel, studentComboBox, studentPropsButton, studentAddButton)
 
   Model.selectedStudent <== studentComboBox.selectionModel().selectedItemProperty()
-  Model.selectedStudent.onChange { studentPropsButton.disable = false }
-  studentPropsButton.onAction = { _ => save(studentComboBox.selectionModel().getSelectedItem) }
-  studentAddButton.onAction = { _ => save(Student()) }
+  Model.selectedStudent.onChange { (_, _, selectedStudent) =>
+    studentPropsButton.disable = false
+    studentComboBox.selectionModel().select(selectedStudent)
+  }
+  studentPropsButton.onAction = { _ => update(studentComboBox.selectionModel().getSelectedIndex, studentComboBox.selectionModel().getSelectedItem) }
+  studentAddButton.onAction = { _ => add(Student()) }
 
-  def save(student: Student): Unit = {
+  def update(selectedIndex: Int, student: Student): Unit = {
     val result = new StudentDialog(student).showAndWait()
     result match {
       case Some(Student(id, name, born)) =>
-        import Store.repository._
+        val student = Student(id, name, born)
+        await(students.save(student))
+        Model.students.update(selectedIndex, student)
+      case _ =>
+    }
+  }
+
+  def add(student: Student): Unit = {
+    val result = new StudentDialog(student).showAndWait()
+    result match {
+      case Some(Student(id, name, born)) =>
         val student = Student(id, name, born)
         val studentId = await(students.save(student))
         val persistedStudent = student.copy(id = studentId.get)
         if (id == 0) {
           Model.students += persistedStudent
-          studentComboBox.selectionModel().select(persistedStudent)
+          Model.selectedStudent.value = persistedStudent
         }
-      case _ => println("Student dialog failed!")
+      case _ =>
     }
   }
 }
