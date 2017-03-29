0ee463c685209ca348016a3ddbddc25af90f56d6
objektwerks <objektwerks@runbox.com>
2017-03-29 18:44:31 -0400

    edited default entity values

--- src/main/scala/hs/entity/Entity.scala
+++ src/main/scala/hs/entity/Entity.scala
@@ -2,7 +2,7 @@ package hs.entity
 
 import java.time.LocalDate
 
-case class Student(id: Int = 0, name: String = "Name?", born: LocalDate = LocalDate.now.minusYears(7))
-case class Grade(id: Int = 0, studentid: Int, year: String = "Year?", started: LocalDate = LocalDate.now, completed: LocalDate = LocalDate.now.plusMonths(6))
-case class Course(id: Int = 0, gradeid: Int, name: String = "Name?")
-case class Assignment(id: Int = 0, courseid: Int, task: String = "Task?", assigned: LocalDate = LocalDate.now, completed: LocalDate = LocalDate.now, score: Double = 0.0)
+case class Student(id: Int = 0, name: String = "default_student", born: LocalDate = LocalDate.now.minusYears(7))
+case class Grade(id: Int = 0, studentid: Int, year: String = "default_grade", started: LocalDate = LocalDate.now, completed: LocalDate = LocalDate.now.plusMonths(6))
+case class Course(id: Int = 0, gradeid: Int, name: String = "default_course")
+case class Assignment(id: Int = 0, courseid: Int, task: String = "default_assignment", assigned: LocalDate = LocalDate.now, completed: LocalDate = LocalDate.now, score: Double = 0.0)
