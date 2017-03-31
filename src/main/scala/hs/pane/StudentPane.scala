8d275e7b5ab750f115db3b6ec61a4a67ba16d11b
objektwerks <objektwerks@runbox.com>
2017-03-31 06:39:53 -0400

    added async scalafx task to student pane

--- src/main/scala/hs/pane/StudentPane.scala
+++ src/main/scala/hs/pane/StudentPane.scala
@@ -1,17 +1,22 @@
 package hs.pane
 
+import javafx.{concurrent => jfxc}
+
 import com.typesafe.config.Config
 import hs.dialog.StudentDialog
 import hs.entity.Student
 import hs.model.Model
 
+import scala.concurrent.ExecutionContext
 import scalafx.Includes._
+import scalafx.concurrent.Task
 import scalafx.scene.control.cell.TextFieldListCell
 import scalafx.scene.control.{Button, Label, ListView}
 import scalafx.scene.layout.HBox
 import scalafx.util.StringConverter
 
 class StudentPane(conf: Config, model: Model) extends HBox {
+  val ec = ExecutionContext.global
   val studentLabel = new Label { text = "Student:" }
   val studentCellFactory = TextFieldListCell.forListView( StringConverter.toStringConverter[Student](s => s.name) )
   val studentListView = new ListView[Student] { prefHeight = 100; prefWidth = 203; items = model.studentList; cellFactory = studentCellFactory }
@@ -32,14 +37,20 @@ class StudentPane(conf: Config, model: Model) extends HBox {
 
   def update(selectedIndex: Int, student: Student): Unit = {
     new StudentDialog(conf, student).showAndWait() match {
-      case Some(Student(id, name, born)) => model.updateStudent(selectedIndex, Student(id, name, born))
+      case Some(Student(id, name, born)) =>
+        ec.execute(Task(new jfxc.Task[Unit] {
+          def call(): Unit  = { model.updateStudent(selectedIndex, Student(id, name, born)) }
+        }))
       case _ =>
     }
   }
 
   def add(student: Student): Unit = {
     new StudentDialog(conf, student).showAndWait() match {
-      case Some(Student(id, name, born)) => model.addStudent(Student(id, name, born))
+      case Some(Student(id, name, born)) =>
+        ec.execute(Task(new jfxc.Task[Unit] {
+          def call(): Unit  = { model.addStudent(Student(id, name, born)) }
+        }))
       case _ =>
     }
   }
