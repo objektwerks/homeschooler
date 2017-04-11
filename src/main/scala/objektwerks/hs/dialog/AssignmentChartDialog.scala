b79daf58ec6fa964d70864e3deeb4f94dbbe8e58
objektwerks <objektwerks@email.com>
2017-04-11 14:47:49 -0400

    building assignment chart dialog

--- src/main/scala/objektwerks/hs/dialog/AssignmentChartDialog.scala
+++ src/main/scala/objektwerks/hs/dialog/AssignmentChartDialog.scala
@@ -14,7 +14,6 @@ import scalafx.scene.layout.{HBox, VBox}
 
 class AssignmentChartDialog(conf: Config, assignments: List[Assignment]) extends Dialog[Unit] {
   val xAxis = NumberAxis(axisLabel = conf.getString("chart-months"), lowerBound = 1, upperBound = 13, tickUnit = 1)
-  xAxis.minorTickCount = 16
   val yAxis = NumberAxis(axisLabel = conf.getString("chart-scores"), lowerBound = 0, upperBound = 100, tickUnit = 10)
   val chart = LineChart[Number, Number](xAxis, yAxis)
   val series = new XYChart.Series[Number, Number]{ name = conf.getString("chart-score") }
