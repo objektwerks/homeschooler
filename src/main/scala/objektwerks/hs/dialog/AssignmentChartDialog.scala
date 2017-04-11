664aad96158fdac89bc6a99fad4c38085ce48b5e
objektwerks <objektwerks@email.com>
2017-04-11 07:00:47 -0400

    added assignment chart dialog

---
+++ src/main/scala/objektwerks/hs/dialog/AssignmentChartDialog.scala
@@ -0,0 +1,9 @@
+package objektwerks.hs.dialog
+
+import objektwerks.hs.entity.Assignment
+
+import scalafx.scene.control.Dialog
+
+class AssignmentChartDialog(assignments: List[Assignment]) extends Dialog[Unit] {
+
+}
