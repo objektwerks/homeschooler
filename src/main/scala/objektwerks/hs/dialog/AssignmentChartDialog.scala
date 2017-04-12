b1018ce358f683e3ab7f71c26f63f22ad72f83b3
objektwerks <objektwerks@email.com>
2017-04-12 07:00:12 -0400

    refactored chart x axis and label

--- src/main/scala/objektwerks/hs/dialog/AssignmentChartDialog.scala
+++ src/main/scala/objektwerks/hs/dialog/AssignmentChartDialog.scala
@@ -13,13 +13,18 @@ import scalafx.scene.control.{ButtonType, Dialog, Label}
 import scalafx.scene.layout.{HBox, VBox}
 
 class AssignmentChartDialog(conf: Config, assignments: List[Assignment]) extends Dialog[Unit] {
-  val xAxis = NumberAxis(axisLabel = conf.getString("chart-months"), lowerBound = 1, upperBound = 12, tickUnit = 1)
+  val monthFormatter = DateTimeFormatter.ofPattern("MM")
+  val minMonth = assignments.map(a => a.completed.format(monthFormatter).toInt).min
+  val maxMonth = assignments.map(a => a.completed.format(monthFormatter).toInt).max
+
+  val xAxis = NumberAxis(axisLabel = s"${conf.getString("chart-months")} [$minMonth - $maxMonth]", lowerBound = minMonth, upperBound = maxMonth + 1, tickUnit = 1)
   val yAxis = NumberAxis(axisLabel = conf.getString("chart-scores"), lowerBound = 0, upperBound = 100, tickUnit = 10)
   val chart = LineChart[Number, Number](xAxis, yAxis)
+
   val series = new XYChart.Series[Number, Number]{ name = conf.getString("chart-score") }
-  val dateFormatter = DateTimeFormatter.ofPattern("MM.dd")
+  val monthDayFormatter = DateTimeFormatter.ofPattern("MM.dd")
   assignments foreach { assignment =>
-    series.data() += XYChart.Data[Number, Number]( assignment.completed.format(dateFormatter).toDouble, assignment.score.toInt )
+    series.data() += XYChart.Data[Number, Number]( assignment.completed.format(monthDayFormatter).toDouble, assignment.score.toInt )
   }
   chart.data = series
 
