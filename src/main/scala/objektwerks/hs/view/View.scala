b7c8a2a289ce61b04fde4e0f35145fdc7c372dac
objektwerks <objektwerks@email.com>
2017-04-20 18:02:31 -0400

    reformatted view code

--- src/main/scala/objektwerks/hs/view/View.scala
+++ src/main/scala/objektwerks/hs/view/View.scala
@@ -10,7 +10,6 @@ import scalafx.scene.control.SplitPane
 import scalafx.scene.layout.{Priority, VBox}
 
 class View(conf: Config, model: Model) {
-  val menuPane = new MenuPane(conf)
   val studentPane = new StudentPane(conf, model)
   val gradePane = new GradePane(conf, model)
   val coursePane = new CoursePane(conf, model)
@@ -18,7 +17,10 @@ class View(conf: Config, model: Model) {
 
   val westPane = new VBox { spacing = 6; padding = Insets(6); children = List(studentPane, coursePane) }
   val eastPane = new VBox { spacing = 6; padding = Insets(6); children = List(gradePane, assignmentPane) }
+
+  val menuPane = new MenuPane(conf)
   val splitPane = new SplitPane { vgrow = Priority.Always; hgrow = Priority.Always; padding = Insets(6); items.addAll(westPane, eastPane) }
+
   val contentPane = new VBox { prefHeight = 600; prefWidth = 800; spacing = 6; padding = Insets(6); children = List(menuPane, splitPane) }
   val sceneGraph = new Scene { root = contentPane }
 
