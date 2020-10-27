cc3bae715a7d315759db792de997d9218dcbfa30
objektwerks <objektwerks@runbox.com>
2020-10-26 20:39:02 -0400

    refactored images

--- src/main/scala/objektwerks/hs/image/Images.scala
+++ src/main/scala/objektwerks/hs/image/Images.scala
@@ -3,11 +3,11 @@ package objektwerks.hs.image
 import scalafx.scene.image.{Image, ImageView}
 
 object Images {
-  val appImage = new Image(Images.getClass.getResourceAsStream("/images/homeschool.png"))
-  val addImageView = loadImageView("/images/add.png")
-  val editImageView = loadImageView("/images/edit.png")
-  val barChartImageView = loadImageView("/images/bar.chart.png")
-  val lineChartImageView = loadImageView("/images/line.chart.png")
+  def appImage = new Image(Images.getClass.getResourceAsStream("/images/homeschool.png"))
+  def addImageView = loadImageView("/images/add.png")
+  def editImageView = loadImageView("/images/edit.png")
+  def barChartImageView = loadImageView("/images/bar.chart.png")
+  def lineChartImageView = loadImageView("/images/line.chart.png")
 
   def loadImageView(path: String): ImageView = new ImageView {
     image = new Image(Images.getClass.getResourceAsStream(path))
