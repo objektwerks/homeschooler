4444375d70159a0a16822b545751c4ae070ef9e1
objektwerks <objektwerks@runbox.com>
2022-04-11 09:04:43 -0400

    upgraded jfxapp to jfxapp3

--- src/main/scala/objektwerks/hs/App.scala
+++ src/main/scala/objektwerks/hs/App.scala
@@ -6,22 +6,25 @@ import objektwerks.hs.model.Model
 import objektwerks.hs.repository.Repository
 import objektwerks.hs.view.View
 
-import scalafx.application.JFXApp
+import scalafx.application.JFXApp3
 
-object App extends JFXApp {
+object App extends JFXApp3 {
   val conf = ConfigFactory.load("app.conf")
   val repository = Repository("repository.conf")
   val model = Model(repository)
   val view = View(conf, model)
-  
-  stage = new JFXApp.PrimaryStage {
-    scene = view.sceneGraph
-    title = conf.getString("title")
-    minHeight = conf.getInt("height").toDouble
-    minWidth = conf.getInt("width").toDouble
-    icons.add(Resources.appImage)
-  }
 
+
+  override def start(): Unit = {
+    stage = new JFXApp3.PrimaryStage {
+      scene = view.sceneGraph
+      title = conf.getString("title")
+      minHeight = conf.getInt("height").toDouble
+      minWidth = conf.getInt("width").toDouble
+      icons.add(Resources.appImage)
+    }
+  }
+  
   sys.addShutdownHook {
     repository.close()
   }
