c80f87adea5c31a246183298c7c17d0ff698bc24
objektwerks <objektwerks@runbox.com>
2022-04-13 08:59:40 -0400

    refactored package and imports

--- src/main/scala/hs/pane/GradePane.scala
+++ src/main/scala/hs/pane/GradePane.scala
@@ -1,11 +1,11 @@
-package objektwerks.hs.pane
+package hs.pane
 
 import com.typesafe.config.Config
 
-import objektwerks.hs.Resources
-import objektwerks.hs.dialog.GradeDialog
-import objektwerks.hs.entity.Grade
-import objektwerks.hs.model.Model
+import hs.Resources
+import hs.dialog.GradeDialog
+import hs.Grade
+import hs.Model
 
 import scalafx.Includes._
 import scalafx.scene.control.{Button, Label, ListView}
