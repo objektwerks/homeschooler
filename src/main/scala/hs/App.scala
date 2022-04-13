e187ce5785112679b52d9ffb8917ca320fb64433
objektwerks <objektwerks@runbox.com>
2022-04-13 09:10:41 -0400

    edited conf name in app

--- src/main/scala/hs/App.scala
+++ src/main/scala/hs/App.scala
@@ -5,18 +5,18 @@ import com.typesafe.config.ConfigFactory
 import scalafx.application.JFXApp3
 
 object App extends JFXApp3 {
-  val conf = ConfigFactory.load("resources.conf")
+  val resources = ConfigFactory.load("resources.conf")
   val repository = Repository("repository.conf")
   val model = Model(repository)
-  val view = View(conf, model)
+  val view = View(resources, model)
 
 
   override def start(): Unit = {
     stage = new JFXApp3.PrimaryStage {
       scene = view.sceneGraph
-      title = conf.getString("title")
-      minHeight = conf.getInt("height").toDouble
-      minWidth = conf.getInt("width").toDouble
+      title = resources.getString("title")
+      minHeight = resources.getInt("height").toDouble
+      minWidth = resources.getInt("width").toDouble
       icons.add(Resources.appImage)
     }
   }
