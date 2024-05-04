363b296b20ffc47e2201b56d0aa9bde7a30c7161
objektwerks <objektwerks@runbox.com>
2024-05-04 09:19:32 -0400

    edited entity

--- src/main/scala/hs/Entity.scala
+++ src/main/scala/hs/Entity.scala
@@ -4,6 +4,14 @@ import java.time.LocalDate
 
 sealed trait Entity extends Product with Serializable
 
+object Entity {
+  implicit def localDateOrdering: Ordering[LocalDate] = Ordering.by(_.toEpochDay)
+  implicit def studentOrdering: Ordering[Student] = Ordering.by(_.born)
+  implicit def gradeOrdering: Ordering[Grade] = Ordering.by(_.started)
+  implicit def courseOrdering: Ordering[Course] = Ordering.by(_.started)
+  implicit def assignmentOrdering: Ordering[Assignment] = Ordering.by(_.assigned)
+}
+
 final case class Student(id: Int = 0,
                          name: String = "default_student",
                          born: LocalDate = LocalDate.now.minusYears(7)) extends Entity
@@ -26,11 +34,3 @@ final case class Assignment(id: Int = 0,
                             assigned: LocalDate = LocalDate.now,
                             completed: LocalDate = LocalDate.now,
                             score: Double = 50.0) extends Entity
-
-object Entity {
-  implicit def localDateOrdering: Ordering[LocalDate] = Ordering.by(_.toEpochDay)
-  implicit def studentOrdering: Ordering[Student] = Ordering.by(_.born)
-  implicit def gradeOrdering: Ordering[Grade] = Ordering.by(_.started)
-  implicit def courseOrdering: Ordering[Course] = Ordering.by(_.started)
-  implicit def assignmentOrdering: Ordering[Assignment] = Ordering.by(_.assigned)
-}
