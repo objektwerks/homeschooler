da2a05d3cdd89744f48c62d558328590add733e5
objektwerks <objektwerks@runbox.com>
2024-05-05 06:56:56 -0400

    refactored student

--- src/main/scala/hs/Entity.scala
+++ src/main/scala/hs/Entity.scala
@@ -12,24 +12,24 @@ object Entity {
 }
 
 final case class Student(id: Int = 0,
-                         name: String = "default_student",
-                         born: LocalDate = LocalDate.now.minusYears(7)) extends Entity
+                         name: String = "",
+                         born: String = LocalDate.now.minusYears(7).toString) extends Entity
 
 final case class Grade(id: Int = 0,
                        studentid: Int,
-                       year: String = "default_grade",
+                       year: String = "",
                        started: String = LocalDate.now.toString,
                        completed: String = LocalDate.now.plusMonths(6).toString) extends Entity
 
 final case class Course(id: Int = 0,
                         gradeid: Int,
-                        name: String = "default_course",
+                        name: String = "",
                         started: String = LocalDate.now.toString,
                         completed: String = LocalDate.now.plusMonths(3).toString) extends Entity
 
 final case class Assignment(id: Int = 0,
                             courseid: Int,
-                            task: String = "default_assignment",
+                            task: String = "",
                             assigned: String = LocalDate.now.toString,
                             completed: String = LocalDate.now.toString,
                             score: Double = 50.0) extends Entity
