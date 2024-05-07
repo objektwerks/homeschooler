582f933fa624ffa3df3639ad2ab479654c9e34d4
objektwerks <objektwerks@runbox.com>
2024-05-07 06:26:40 -0400

    reformatted

--- src/main/scala/hs/dialog/CourseDialog.scala
+++ src/main/scala/hs/dialog/CourseDialog.scala
@@ -2,13 +2,13 @@ package hs.dialog
 
 import com.typesafe.config.Config
 
-import hs.{App, Course, Entity}
-import hs.pane.ControlGridPane
-
 import scalafx.Includes._
 import scalafx.scene.control.ButtonBar.ButtonData
 import scalafx.scene.control._
 import scalafx.scene.layout.Region
+
+import hs.{App, Course, Entity}
+import hs.pane.ControlGridPane
 
 class CourseDialog(conf: Config, course: Course) extends Dialog[Course]:
   val nameTextField = new TextField:
@@ -38,13 +38,12 @@ class CourseDialog(conf: Config, course: Course) extends Dialog[Course]:
     saveButton.disable = newValue.trim.isEmpty
   }
 
-  resultConverter = dialogButton => {
+  resultConverter = dialogButton =>
     if (dialogButton == saveButtonType)
       course.copy(name = nameTextField.text.value,
         started = startedDatePicker.value.value.toString,
         completed = completedDatePicker.value.value.toString)
     else null
-  }
 
   initOwner(App.stage)
   title = conf.getString("course")
