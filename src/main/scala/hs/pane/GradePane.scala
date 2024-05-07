bb7f944057f7c873dcd647798e1b306cb2b89448
objektwerks <objektwerks@runbox.com>
2024-05-07 06:50:39 -0400

    reformatted

--- src/main/scala/hs/pane/GradePane.scala
+++ src/main/scala/hs/pane/GradePane.scala
@@ -2,33 +2,30 @@ package hs.pane
 
 import com.typesafe.config.Config
 
-import hs.Images
-import hs.dialog.GradeDialog
-import hs.Grade
-import hs.Model
-
-import scalafx.Includes._
+import scalafx.Includes.*
 import scalafx.scene.control.{Button, Label, ListView}
 import scalafx.scene.layout.{HBox, VBox}
 
-class GradePane(conf: Config, model: Model) extends VBox {
-  val gradeLabel = new Label {
+import hs.{Grade, Images, Model}
+import hs.dialog.GradeDialog
+
+class GradePane(conf: Config, model: Model) extends VBox:
+  val gradeLabel = new Label:
     text = conf.getString("grades")
-  }
-  val gradeListView = new ListView[Grade] {
+
+  val gradeListView = new ListView[Grade]:
     minHeight = 50
     items = model.gradeList
     cellFactory = (cell, grade) => { cell.text =  grade.year }
-  }
-  val gradeAddButton = new Button {
+
+  val gradeAddButton = new Button:
     graphic = Images.addImageView; prefHeight = 25; disable = true
-  }
-  val gradeEditButton = new Button {
+
+  val gradeEditButton = new Button:
     graphic = Images.editImageView; prefHeight = 25; disable = true
-  }
-  val gradeToolBar = new HBox {
+
+  val gradeToolBar = new HBox:
     spacing = 6; children = List(gradeAddButton, gradeEditButton)
-  }
 
   spacing = 6
   children = List(gradeLabel, gradeListView, gradeToolBar)
@@ -40,10 +37,9 @@ class GradePane(conf: Config, model: Model) extends VBox {
 
   gradeListView.selectionModel().selectedItemProperty().onChange { (_, _, selectedGrade) =>
     // model.update executes a remove and add on items. the remove passes a null selectedGrade!
-    if (selectedGrade != null) {
+    if (selectedGrade != null) then
       model.selectedGradeId.value = selectedGrade.id
       gradeEditButton.disable = false
-    }
   }
 
   gradeListView.onMouseClicked = { event =>
@@ -54,23 +50,18 @@ class GradePane(conf: Config, model: Model) extends VBox {
 
   gradeEditButton.onAction = { _ => update() }
 
-  def add(grade: Grade): Unit = {
-    new GradeDialog(conf, grade).showAndWait() match {
+  def add(grade: Grade): Unit =
+    new GradeDialog(conf, grade).showAndWait() match
       case Some(Grade(id, studentid, year, started, completed)) =>
         val newGrade = model.addGrade(Grade(id, studentid, year, started, completed))
         gradeListView.selectionModel().select(newGrade)
       case _ =>
-    }
-  }
 
-  def update(): Unit = {
+  def update(): Unit =
     val selectedIndex = gradeListView.selectionModel().getSelectedIndex
     val grade = gradeListView.selectionModel().getSelectedItem
-    new GradeDialog(conf, grade).showAndWait() match {
+    new GradeDialog(conf, grade).showAndWait() match
       case Some(Grade(id, studentid, year, started, completed)) =>
         model.updateGrade(selectedIndex, Grade(id, studentid, year, started, completed))
         gradeListView.selectionModel().select(selectedIndex)
       case _ =>
-    }
-  }
-}
