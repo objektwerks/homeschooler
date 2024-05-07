5e981188f06bc3838d70ab70668d95c8aabc0aee
objektwerks <objektwerks@runbox.com>
2024-05-07 06:47:58 -0400

    reformatted

--- src/main/scala/hs/pane/CoursePane.scala
+++ src/main/scala/hs/pane/CoursePane.scala
@@ -2,37 +2,34 @@ package hs.pane
 
 import com.typesafe.config.Config
 
-import hs.Images
-import hs.dialog.{CourseChartDialog, CourseDialog}
-import hs.Course
-import hs.Model
-
-import scalafx.Includes._
+import scalafx.Includes.*
 import scalafx.scene.control.{Button, Label, ListView, SelectionMode}
 import scalafx.scene.layout.{HBox, VBox}
 
-class CoursePane(conf: Config, model: Model) extends VBox {
-  val courseLabel = new Label {
+import hs.{Course, Images, Model}
+import hs.dialog.{CourseChartDialog, CourseDialog}
+
+class CoursePane(conf: Config, model: Model) extends VBox:
+  val courseLabel = new Label:
     text = conf.getString("courses")
-  }
-  val courseListView = new ListView[Course] {
+
+  val courseListView = new ListView[Course]:
     minHeight = 300;
     items = model.courseList;
     cellFactory = (cell, course) => { cell.text =  course.name }
     selectionModel().selectionMode = SelectionMode.Single
-  }
-  val courseAddButton = new Button {
+
+  val courseAddButton = new Button:
     graphic = Images.addImageView; prefHeight = 25; disable = true
-  }
-  val courseEditButton = new Button {
+
+  val courseEditButton = new Button:
     graphic = Images.editImageView; prefHeight = 25; disable = true
-  }
-  val courseChartButton = new Button {
+
+  val courseChartButton = new Button:
     graphic = Images.barChartImageView; prefHeight = 25; disable = true
-  }
-  val courseToolBar = new HBox {
+
+  val courseToolBar = new HBox:
     spacing = 6; children = List(courseAddButton, courseEditButton, courseChartButton)
-  }
 
   spacing = 6
   children = List(courseLabel, courseListView, courseToolBar)
@@ -40,16 +37,15 @@ class CoursePane(conf: Config, model: Model) extends VBox {
   model.selectedGradeId.onChange { (_, _, selectedGradeId) =>
     model.listCourses(selectedGradeId.intValue)
     courseAddButton.disable = false
-    courseChartButton.disable = if (model.courseList.nonEmpty) false else true
+    courseChartButton.disable = if (model.courseList.nonEmpty) then false else true
   }
 
   courseListView.selectionModel().selectedItemProperty().onChange { (_, _, selectedCourse) =>
     // model.update executes a remove and add on items. the remove passes a null selectedCourse!
-    if (selectedCourse != null) {
+    if (selectedCourse != null) then
       model.selectedCourseId.value = selectedCourse.id
       courseEditButton.disable = false
       courseChartButton.disable = false
-    }
   }
 
   courseListView.onMouseClicked = { event =>
@@ -65,23 +61,18 @@ class CoursePane(conf: Config, model: Model) extends VBox {
     ()
   }
 
-  def add(course: Course): Unit = {
-    new CourseDialog(conf, course).showAndWait() match {
+  def add(course: Course): Unit =
+    new CourseDialog(conf, course).showAndWait() match
       case Some(Course(id, gradeid, name, started, completed)) =>
         val newCourse = model.addCourse(Course(id, gradeid, name, started, completed))
         courseListView.selectionModel().select(newCourse)
       case _ =>
-    }
-  }
 
-  def update(): Unit = {
+  def update(): Unit =
     val selectedIndex = courseListView.selectionModel().getSelectedIndex
     val course = courseListView.selectionModel().getSelectedItem
-    new CourseDialog(conf, course).showAndWait() match {
+    new CourseDialog(conf, course).showAndWait() match
       case Some(Course(id, gradeid, name, started, completed)) =>
         model.updateCourse(selectedIndex, Course(id, gradeid, name, started, completed))
         courseListView.selectionModel().select(selectedIndex)
       case _ =>
-    }
-  }
-}
