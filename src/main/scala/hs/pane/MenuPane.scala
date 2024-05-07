0b32af4879574377ecf48ddfbbf74ae68b9e242b
objektwerks <objektwerks@runbox.com>
2024-05-07 06:53:34 -0400

    reformatted

--- src/main/scala/hs/pane/MenuPane.scala
+++ src/main/scala/hs/pane/MenuPane.scala
@@ -2,37 +2,32 @@ package hs.pane
 
 import com.typesafe.config.Config
 
+import scalafx.application.Platform
+import scalafx.scene.control.{Alert, Menu, MenuBar, MenuItem, SeparatorMenuItem}
+import scalafx.scene.control.Alert.AlertType
+
 import hs.App
 
-import scalafx.application.Platform
-import scalafx.scene.control.Alert.AlertType
-import scalafx.scene.control._
-
-class MenuPane(conf: Config) extends MenuBar {
-  val aboutDialog = new Alert(AlertType.Information) {
+class MenuPane(conf: Config) extends MenuBar:
+  val aboutDialog = new Alert(AlertType.Information):
     initOwner(App.stage)
     title = conf.getString("about")
     headerText = conf.getString("developer")
     contentText = s"${conf.getString("app")} ${conf.getString("license")}"
-  }
 
-  val aboutMenuItem = new MenuItem(conf.getString("about")) {
+  val aboutMenuItem = new MenuItem(conf.getString("about")):
     onAction = { _ =>
       aboutDialog.showAndWait()
       ()
     }
-  }
 
   val separator = new SeparatorMenuItem()
 
-  val exitMenuItem = new MenuItem(conf.getString("exit")) {
+  val exitMenuItem = new MenuItem(conf.getString("exit")):
     onAction = { _ => Platform.exit() }
-  }
   
-  val menu = new Menu(conf.getString("menu")) {
+  val menu = new Menu(conf.getString("menu")):
     items = List(aboutMenuItem, separator, exitMenuItem)
-  }
 
   menus = List(menu)
   useSystemMenuBar = false
-}
