81ce261160cf07029a84522426c04729c1f9590f
objektwerks <objektwerks@runbox.com>
2024-05-08 08:13:51 -0400

    reformatted

--- src/main/scala/hs/pane/GradePane.scala
+++ src/main/scala/hs/pane/GradePane.scala
@@ -51,7 +51,7 @@ class GradePane(conf: Config, model: Model) extends VBox:
   gradeEditButton.onAction = { _ => update() }
 
   def add(grade: Grade): Unit =
-    new GradeDialog(conf, grade).showAndWait() match
+    GradeDialog(conf, grade).showAndWait() match
       case Some(Grade(id, studentid, year, started, completed)) =>
         val newGrade = model.addGrade(Grade(id, studentid, year, started, completed))
         gradeListView.selectionModel().select(newGrade)
@@ -60,7 +60,7 @@ class GradePane(conf: Config, model: Model) extends VBox:
   def update(): Unit =
     val selectedIndex = gradeListView.selectionModel().getSelectedIndex
     val grade = gradeListView.selectionModel().getSelectedItem
-    new GradeDialog(conf, grade).showAndWait() match
+    GradeDialog(conf, grade).showAndWait() match
       case Some(Grade(id, studentid, year, started, completed)) =>
         model.updateGrade(selectedIndex, Grade(id, studentid, year, started, completed))
         gradeListView.selectionModel().select(selectedIndex)
