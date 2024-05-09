d7c0cd25c68c08f17dc579069bb306d19779c6e2
objektwerks <objektwerks@runbox.com>
2024-05-09 06:23:25 -0400

    reformatted

--- src/main/scala/hs/dialog/AssignmentChartDialog.scala
+++ src/main/scala/hs/dialog/AssignmentChartDialog.scala
@@ -15,14 +15,16 @@ import scalafx.scene.layout.{HBox, VBox}
 import hs.{App, Assignment}
 
 class AssignmentChartDialog(conf: Config, assignments: ObservableBuffer[Assignment]) extends Dialog[Unit]:
-  implicit def localDateOrdering: Ordering[LocalDate] = Ordering.by(_.toEpochDay)
+  given Ordering[LocalDate] = Ordering.by(_.toEpochDay)
 
   val dateFormatter = DateTimeFormatter.ofPattern("yy.D")
   val minDate = assignments.map(a => a.completed).min.format(dateFormatter).toDouble
   val maxDate = assignments.map(a => a.completed).max.format(dateFormatter).toDouble
 
   val xAxis = NumberAxis(axisLabel = s"${conf.getString("assignment-chart-months")} [$minDate - $maxDate]", lowerBound = minDate, upperBound = maxDate, tickUnit = 1)
+  
   val yAxis = NumberAxis(axisLabel = conf.getString("assignment-chart-scores"), lowerBound = 0, upperBound = 100, tickUnit = 10)
+
   val chart = LineChart[Number, Number](xAxis, yAxis)
   chart.padding = Insets(6)
 
@@ -32,6 +34,7 @@ class AssignmentChartDialog(conf: Config, assignments: ObservableBuffer[Assignme
   assignments foreach { assignment =>
     series.data() += XYChart.Data[Number, Number](assignment.completed.format(dateFormatter).toDouble, assignment.score.toInt)
   }
+
   chart.data = series
 
   val minScoreLabel = new Label:
@@ -53,12 +56,12 @@ class AssignmentChartDialog(conf: Config, assignments: ObservableBuffer[Assignme
     text = (assignments.map(a => a.score).sum / assignments.length).toInt.toString
 
   val scoreBox = new HBox:
-    alignment = Pos.Center;
-    spacing = 6;
+    alignment = Pos.Center
+    spacing = 6
     children = List(minScoreLabel, minScore, maxScoreLabel, maxScore, scoreLabel, score)
   
   val chartBox = new VBox:
-    spacing = 6;
+    spacing = 6
     children = List(chart, scoreBox)
 
   val dialog = dialogPane()
