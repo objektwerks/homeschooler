f7e2d54f8ee52b3d69f2fe65e390738d2ef4f920
objektwerks <objektwerks@runbox.com>
2024-05-09 06:32:01 -0400

    reformatted

--- src/main/scala/hs/Images.scala
+++ src/main/scala/hs/Images.scala
@@ -3,14 +3,18 @@ package hs
 import scalafx.scene.image.{Image, ImageView}
 
 object Images:
-  def appImage = new Image(Images.getClass.getResourceAsStream("/images/homeschool.png"))
+  def appImage = Image( Images.getClass.getResourceAsStream("/images/homeschool.png") )
+
   def addImageView = loadImageView("/images/add.png")
+
   def editImageView = loadImageView("/images/edit.png")
+
   def barChartImageView = loadImageView("/images/bar.chart.png")
+
   def lineChartImageView = loadImageView("/images/line.chart.png")
 
   def loadImageView(path: String): ImageView = new ImageView:
-    image = new Image(Images.getClass.getResourceAsStream(path))
+    image = Image( Images.getClass.getResourceAsStream(path) )
     fitHeight = 25
     fitWidth = 25
     preserveRatio = true
