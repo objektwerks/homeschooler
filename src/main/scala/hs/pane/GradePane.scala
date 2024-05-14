3dce57656261618d2922809ca83bb2ea861771db
objektwerks <objektwerks@runbox.com>
2024-05-14 13:40:25 -0400

    enhanced grade pane

--- src/main/scala/hs/pane/GradePane.scala
+++ src/main/scala/hs/pane/GradePane.scala
@@ -1,17 +1,15 @@
 package hs.pane
 
-import com.typesafe.config.Config
-
 import scalafx.Includes.*
 import scalafx.scene.control.{Button, Label, ListView}
 import scalafx.scene.layout.{HBox, VBox}
 
-import hs.{Grade, context, Model}
+import hs.{Context, Grade, Model}
 import hs.dialog.GradeDialog
 
-class GradePane(conf: Config, model: Model) extends VBox:
+class GradePane(context: Context, model: Model) extends VBox:
   val gradeLabel = new Label:
-    text = conf.getString("grades")
+    text = context.grades
 
   val gradeListView = new ListView[Grade]:
     minHeight = 50
@@ -57,7 +55,7 @@ class GradePane(conf: Config, model: Model) extends VBox:
   gradeEditButton.onAction = { _ => update() }
 
   def add(grade: Grade): Unit =
-    GradeDialog(conf, grade).showAndWait() match
+    GradeDialog(context, grade).showAndWait() match
       case Some(Grade(id, studentid, year, started, completed)) =>
         val newGrade = model.addGrade(
           Grade(id, studentid, year, started, completed)
@@ -68,7 +66,7 @@ class GradePane(conf: Config, model: Model) extends VBox:
   def update(): Unit =
     val selectedIndex = gradeListView.selectionModel().getSelectedIndex
     val grade = gradeListView.selectionModel().getSelectedItem
-    GradeDialog(conf, grade).showAndWait() match
+    GradeDialog(context, grade).showAndWait() match
       case Some(Grade(id, studentid, year, started, completed)) =>
         model.updateGrade(
           selectedIndex,
