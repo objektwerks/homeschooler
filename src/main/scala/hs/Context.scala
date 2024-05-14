a933313eb0f329bd9377b7d98e60924f20ff0631
objektwerks <objektwerks@runbox.com>
2024-05-14 13:13:10 -0400

    enhanced context

--- src/main/scala/hs/Context.scala
+++ src/main/scala/hs/Context.scala
@@ -23,6 +23,7 @@ final class Context(config: Config):
   val save = config.getString("save")
 
   val courseChartCourses = config.getString("course-chart-courses")
+  val courseChartScores = config.getString("course-chart-scores")
 
   def appImage = Image( Images.getClass.getResourceAsStream("/images/homeschool.png") )
 
