42682d661c47c6cbdbcde39ebefca9cc39a82bbf
objektwerks <objektwerks@runbox.com>
2024-05-18 15:51:57 -0400

    edited ddl.sql

--- ddl.sql
+++ ddl.sql
@@ -1,30 +1,30 @@
 create table students (
   id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR NOT NULL,
-  born DATE NOT NULL
+  born VARCHAR NOT NULL
 );
 create table grades (
   id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
   student_id INTEGER NOT NULL,
   grade VARCHAR NOT NULL,
-  started DATE NOT NULL,
-  completed DATE NOT NULL,
+  started VARCHAR NOT NULL,
+  completed VARCHAR NOT NULL,
   foreign key(student_id) references students(id)
 );
 create table courses (
   id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
   grade_id INTEGER NOT NULL,
   name VARCHAR NOT NULL UNIQUE,
-  started DATE NOT NULL,
-  completed DATE NOT NULL,
+  started VARCHAR NOT NULL,
+  completed VARCHAR NOT NULL,
   foreign key(grade_id) references grades(id)
 );
 create table assignments (
   id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
   course_id INTEGER NOT NULL,
   task VARCHAR NOT NULL,
-  assigned DATE NOT NULL,
-  completed DATE NOT NULL,
+  assigned VARCHAR NOT NULL,
+  completed VARCHAR NOT NULL,
   score DOUBLE NOT NULL,
   foreign key(course_id) references courses(id)
 );
